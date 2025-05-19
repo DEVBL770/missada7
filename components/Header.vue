0ccc94661b5ec363edc4b3d82a3e37e99d47c894
Votre Nom <befrigeo@gmail.com>
2025-05-19 16:34:59 +0200

    Initial commit 3

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="text-center py-12 bg-ivory">
       <img src="https://static.vecteezy.com/system/resources/thumbnails/023/654/784/small_2x/golden-logo-template-free-png.png" alt="Logo" class="mx-auto w-28 h-28 mb-6" />
-      <h1 class="text-4xl font-serif text-softblack">Chez Bon Goût</h1>
+      <h1 class="text-4xl font-serif text-softblack">Mon Resto</h1>
       <p class="text-lg font-light text-gray-600 mt-2">Carte raffinée — Cuisine française de saison</p>
     </header>
   </template>
